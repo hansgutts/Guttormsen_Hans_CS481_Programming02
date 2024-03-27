e3ab8e45bc9a2ae398c816925abab2e14d95efe2
hansgutts <hansgutts@gmail.com>
2024-03-27 16:56:25 -0500

    Remove drug from review and add comments

--- CleanCSV.py
+++ CleanCSV.py
@@ -7,28 +7,45 @@ csv1path = "UCIdrug_test.csv"
 csv2path = "UCIdrug_train.csv"
 newcsvpath = "UCIdrugClean.csv"
 
+#a list of our new csv file
 newcsv = []
 
+#open both csv files
 with open(csv1path, errors='ignore') as csv1 :
     with open(csv2path, errors='ignore') as csv2 :
+        
+        #skip header
         csv1reader = csv.reader(csv1)
         next(csv1reader, None)
 
+        #skip header
         csv2reader = csv.reader(csv2)
         next(csv2reader, None)
 
+        #variables to keep track of data
         i = 0
         currline = ''
         csv2len = 0
         csv1len = 0
 
+        #counts of positive and negative documents
         pos = 0
         neg = 0
-        try :
+
+        try :#certain lines might error
             
+            #go through the first csv
             for line in csv1reader :
+                #newline is the new csv value
                 newline = []
+
+                #get the name of the drug (want to remove it to make sure it isn't just associating certain drugs with pos/neg)
+                drug = line[1].lower()
+
+                #extract rating from old one
                 rating = line[4]
+
+                #convert 1-10 rating to pos or neg review
                 if int(rating) >= 7 :
                     pos += 1
                     rating = 1
@@ -36,20 +53,31 @@ with open(csv1path, errors='ignore') as csv1 :
                     neg += 1
                     rating = 0
 
-                currline = html.unescape(line[3])
-                currline = currline.replace('/', ' ').replace('.', ' ').replace('-', ' ')
+                #convert html tags to matching punctuation
+                currline = html.unescape(line[3]).lower()
 
-                currline = currline.translate(str.maketrans('', '', string.punctuation)).lower()
+                #clean up certain punctuation and remove the name of the drug
+                currline = currline.replace('/', ' ').replace('.', ' ').replace('-', ' ').replace(drug, '')
 
+                #remove the rest of the punctuation
+                currline = currline.translate(str.maketrans('', '', string.punctuation))
+
+                #create the csv value with rating and review
                 newline = [rating, currline]
 
+                #add it to out file
                 newcsv.append(newline)
+
+                #count len of file
                 csv1len += 1
 
-            
+            #same as above but for second csv file
             for line in csv2reader :
 
                 newline = []
+
+                drug = line[1].lower()
+
                 rating = line[4]
                 if int(rating) >= 7:
                     pos += 1
@@ -57,8 +85,8 @@ with open(csv1path, errors='ignore') as csv1 :
                 else :
                     neg += 1
                     rating = 0
-                currline = html.unescape(line[3])
-                currline = currline.replace('/', ' ').replace('.', ' ').replace('-', ' ')
+                currline = html.unescape(line[3]).lower()
+                currline = currline.replace('/', ' ').replace('.', ' ').replace('-', ' ').replace(drug, '')
 
                 currline = currline.translate(str.maketrans('', '', string.punctuation)).lower()
 
